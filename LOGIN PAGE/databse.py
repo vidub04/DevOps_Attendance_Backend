14ac6d7b3d018ec8574818bb5ee51ef0b25aa7d3
unknown <vidub04@gmail.com>
2026-04-13 19:02:21 +0530

    variables hidden

--- LOGIN PAGE/databse.py
+++ LOGIN PAGE/databse.py
@@ -1,18 +1,22 @@
 from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
 
-# 🔑 Supabase credentials
-url = "https://befrnamptjppdadsrrpo.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJlZnJuYW1wdGpwcGRhZHNycnBvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTkwNjE3NywiZXhwIjoyMDkxNDgyMTc3fQ.WMN3xCKnUNDmKYlQksZWcxRb1eHlQ13H4azyDiOi-NE"
+load_dotenv()
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
 
 supabase: Client = create_client(url, key)
 
 # 📦 Data to insert
 data = {
-    "Enrolment_Number": "07701192024",
-    "Name_of_Student": "Vanshika Yadav",
+    "Enrolment_Number": "07801192024",
+    "Name_of_Student": "Vidushi bhardwaj",
     "Branch": "AIML",
-    "College_Email": "abc@igdtuw.com",
-    "Semester":"4"
+    "College_Email": "def@igdtuw.com",
+    "Semester":"4",
+    "Password":"goooodoo"
 }
 
 # 🚀 Insert into table
